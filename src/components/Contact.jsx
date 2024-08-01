cb127f11f528e39d7f50b950e9f7ba0cde3e3c51
shannu shaik <shannushaik080@gmail.com>
2024-08-01 14:51:17 +0530

    Initial commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,9 @@ import { styles } from '../styles';
 import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
-
+//8yaGFYqKICXw3uZrx
+//template_g1f5fkt
+//service_cdbd7pr
 
 const Contact = () => {
   const formRef = useRef();
@@ -17,10 +19,38 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
   const handleChange = (e) => {
+    const { name, value } = e.target;
 
+    setForm({ ...form, [name]: value })
   }
   const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
 
+    emailjs.send("service_cdbd7pr",
+      "template_g1f5fkt",
+      {
+        form_name: form.name,
+        to_name: 'Davood',
+        from_email: form.email,
+        to_email: 'shaikshannu757@gmail.com',
+        message: form.message,
+      },
+      '8yaGFYqKICXw3uZrx'
+    )
+      .then(() => {
+        setLoading(false);
+        alert('Hey buddy Thank you for sending I will get back to you soon');
+        setForm({
+          name: "",
+          email: '',
+          message: '',
+        })
+      },(error)=>{
+        setLoading(false)
+        console.log(error);
+        alert('Bad day, something went wrong buddy..')
+      })
   }
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
@@ -39,7 +69,7 @@ const Contact = () => {
           </lable>
           <lable className='flex flex-col'>
             <span className="text-white font-medium mb-4">Your Email</span>
-            <input type="text" name="Email" value={form.email} onChange={handleChange} placeholder="What's your email?" className="bg-tertiary py-4 px-6 palceholder:text-secondary text-white rounded-lg outline-none border-none font-medium" />
+            <input type="email" name="email" value={form.email} onChange={handleChange} placeholder="What's your email?" className="bg-tertiary py-4 px-6 palceholder:text-secondary text-white rounded-lg outline-none border-none font-medium" />
           </lable>
           <lable className='flex flex-col'>
             <span className="text-white font-medium mb-4">Your Message</span>
@@ -54,8 +84,8 @@ const Contact = () => {
         </form>
       </motion.div>
       <motion.div variants={slideIn('right', 'tween', 0.2, 1)}
-      className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px] ">
-          <EarthCanvas />
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px] ">
+        <EarthCanvas />
       </motion.div>
     </div>
   )
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Davood_Portfolio1/",
+  base:"/Davood_New_Portfolio/",
   plugins: [react()],
 })
